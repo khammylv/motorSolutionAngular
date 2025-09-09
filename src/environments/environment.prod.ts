d36d30a766cf7f140efb7b0d82c43d5d14a60145
maria camila leal <khammylv@gmail.com>
2025-09-09 13:43:42 -0500

    feat/add https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://MotorSolution.somee.com/api'
+  apiUrl: 'https://MotorSolution.somee.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://MotorSolution.somee.com/api'
+  apiUrl: 'https://MotorSolution.somee.com/api'
 };
